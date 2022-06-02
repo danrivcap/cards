dd2c90057ca7c3fd657cafd60ced5b4a39387dc8
Daniel Rivero Capellán <danrivcap@gmail.com>
2022-06-02 12:30:51 +0200

    Dummy implementation of dealing a card in sequence

--- src/main/java/es/danrivcap/cv/houseofcards/app/impl/RandomPokerDeck.java
+++ src/main/java/es/danrivcap/cv/houseofcards/app/impl/RandomPokerDeck.java
@@ -1,10 +1,15 @@
 package es.danrivcap.cv.houseofcards.app.impl;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.Optional;
+import java.util.Set;
 
 import es.danrivcap.cv.houseofcards.app.Deck;
 import es.danrivcap.cv.houseofcards.model.Card;
+import es.danrivcap.cv.houseofcards.model.Face;
+import es.danrivcap.cv.houseofcards.model.Suit;
 import es.danrivcap.cv.houseofcards.model.poker.PokerFace;
 import es.danrivcap.cv.houseofcards.model.poker.PokerSuit;
 
@@ -13,7 +18,7 @@ import es.danrivcap.cv.houseofcards.model.poker.PokerSuit;
  * it implement concrete Poker Cards 
  * it deals card randomly  
  **/
-public class RandomPokerDeck implements Deck<PokerSuit,PokerFace>{
+public class RandomPokerDeck implements Deck<Suit,Face>{
 
 	private static final int EMPTY = 0;
 	/*This is the default size for a poker deck but there are packaged
@@ -25,13 +30,23 @@ public class RandomPokerDeck implements Deck<PokerSuit,PokerFace>{
 	private int deckSize = DEFAULT_POKER_DECK_SIZE;
 	
 	//Deck holder where cards lives, I preffer ArrayList over Arrays due to API Flexibility and generic type system facilities. 
-	private ArrayList<Card<PokerSuit,PokerFace>> deck;
+	private ArrayList<Card<Suit,Face>> deck;
 	
 	/**
 	 * Initializes the deck so it is full of cards and its default values 
 	 **/
 	public RandomPokerDeck() {
 		deck = new ArrayList<>(DEFAULT_POKER_DECK_SIZE);
+		Set<PokerSuit> suits = EnumSet.allOf(PokerSuit.class);
+		Set<PokerFace> faces = EnumSet.allOf(PokerFace.class);
+		for (PokerSuit suit : suits) {
+			for (PokerFace face : faces) {
+				Card<Suit, Face> card = new Card<>(suit,face);
+				deck.add(card);
+			}
+		}
+		
+		
 	}
 	
 	/***
@@ -52,10 +67,10 @@ public class RandomPokerDeck implements Deck<PokerSuit,PokerFace>{
 	 * The dealer is external because you can select between different deal strategies random, in order, based in a predicate, 
 	 * in this class is fixed but in ohters you can choose in construction time.
 	 ***/
-	public Optional<Card<PokerSuit, PokerFace>> dealOneCard() {
+	public Optional<Card<Suit, Face>> dealOneCard() {
 		if(deckSize != EMPTY){
 			deckSize--;
-			return Optional.of(deck.get(0));
+			return Optional.of(deck.get(deckSize));
 		}else {
 			return Optional.empty();
 		}
--- src/main/java/es/danrivcap/cv/houseofcards/client/ClientApp.java
+++ src/main/java/es/danrivcap/cv/houseofcards/client/ClientApp.java
@@ -1,6 +1,12 @@
 package es.danrivcap.cv.houseofcards.client;
 
+import java.util.Optional;
+
+import es.danrivcap.cv.houseofcards.app.Deck;
+import es.danrivcap.cv.houseofcards.app.impl.RandomPokerDeck;
 import es.danrivcap.cv.houseofcards.model.Card;
+import es.danrivcap.cv.houseofcards.model.Face;
+import es.danrivcap.cv.houseofcards.model.Suit;
 import es.danrivcap.cv.houseofcards.model.poker.PokerSuit;
 import es.danrivcap.cv.houseofcards.model.poker.PokerFace;
 
@@ -10,9 +16,14 @@ public class ClientApp {
 	 * Client application which acts as a test for the specification of the problem
 	 **/
 	public static void main(String[] args) {
-		Card card = new Card<PokerSuit,PokerFace>(PokerSuit.CLUB,PokerFace.TWO);
+		Deck<Suit,Face> deck = new RandomPokerDeck();
 
-		System.out.println(card);
+		Optional<Card<Suit, Face>> optionalCard = deck.dealOneCard();
+		 optionalCard = deck.dealOneCard();
+		
+		 
+		 
+		System.out.println(optionalCard.get());
 	}
 
 }
